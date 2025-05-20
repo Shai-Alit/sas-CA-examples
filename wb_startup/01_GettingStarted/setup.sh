42534f04d6c5480d0c0179c0654cc0928639f427
Sean T Ford <sean.ford@sas.com>
2025-05-20 15:34:27 -0500

    updated setup script

--- wb_startup/01_GettingStarted/setup.sh
+++ wb_startup/01_GettingStarted/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#first, put this code in a terminal -> chmod -R 777 setup.sh
+#STEP 1: put this code in a terminal -> chmod -R 777 setup.sh
+#STEP 2: type in the terminal -> ./setup.sh
 
 mkdir $WORKSPACE/sasfiles
 mkdir $WORKSPACE/data
@@ -8,9 +9,10 @@ mkdir $WORKSPACE/github
 
 #clone desired repos
 git -C $WORKSPACE/github clone https://github.com/sassoftware/sas-viya-workbench-examples.git 
+git -C $WORKSPACE/github clone https://github.com/Shai-Alit/sas-CA-examples.git
 
 #copy over autoexec
-cp autoexec.sas /home/sas/autoexec.sas
+cp $WORKSPACE/github/sas-CA-examples/01_GettingStarted/autoexec.sas /home/sas/autoexec.sas
 
 #download desired extensions that want to manually install
 wget https://github.com/microsoft/vscode-azurestorage/releases/download/v0.16.2/vscode-azurestorage-0.16.2.vsix -P $WORKSPACE/extensions
