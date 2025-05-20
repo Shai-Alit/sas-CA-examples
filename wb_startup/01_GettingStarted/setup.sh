62f4c10fde898449973dc078ff95a5c157376d3e
Sean F <54478616+Shai-Alit@users.noreply.github.com>
2025-05-20 15:36:32 -0500

    Update setup.sh

--- wb_startup/01_GettingStarted/setup.sh
+++ wb_startup/01_GettingStarted/setup.sh
@@ -14,9 +14,5 @@ git -C $WORKSPACE/github clone https://github.com/Shai-Alit/sas-CA-examples.git
 #copy over autoexec
 cp $WORKSPACE/github/sas-CA-examples/01_GettingStarted/autoexec.sas /home/sas/autoexec.sas
 
-#download desired extensions that want to manually install
-wget https://github.com/microsoft/vscode-azurestorage/releases/download/v0.16.2/vscode-azurestorage-0.16.2.vsix -P $WORKSPACE/extensions
-
 #install python requirements
-pip install -r reqruirements.txt
-
+pip install -r $WORKSPACE/github/sas-CA-examples/01_GettingStarted/requirements.txt
